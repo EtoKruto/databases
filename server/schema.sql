17ae322eeb8d48a9980ab6883c19e768679a9cbb
gilcohen67 <gil.cohen67@gmail.com>
2022-05-04 17:18:13 -0700

    getting errors on first test

--- server/schema.sql
+++ server/schema.sql
@@ -1,13 +1,22 @@
-CREATE DATABASE chat;
 
+-- CREATE DATABASE chat;
+-- DROP TABLE [IF EXISTS] chat;
 USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  username varchar(25) not null,
+  message varchar(140) not null,
+  roomname varchar(25) not null
 );
 
 /* Create other tables and define schemas for them here! */
 
+--  id  INT unsigned NOT NULL AUTO_INCREMENT, # Unique ID
+--    ->   title  VARCHAR(150) NOT NULL,                # Name of book title
+--    ->   author  VARCHAR(150) NOT NULL,                # Title of the Author
+--    ->   pages  SMALLINT UNSIGNED,                        # Pages in the book
+--    ->   PRIMARY KEY     (id)                         # ID = Primary key
 
 
 
@@ -15,3 +24,4 @@ CREATE TABLE messages (
  *    mysql -u root < server/schema.sql
  *  to create the database and the tables.*/
 
+-- you find a bug in the schema or how it was generated, you'll want to "drop" all the new tables before running it again. This will reset your database by throwing away all data and schema information
